87138baa27eeac5d3c3d6d267d898f2ee61cb0dd
Giedrius Rimkus <info@giedriusr.lt>
2024-06-13 16:47:18 +0300

    Page model with avo install

---
+++ app/avo/resources/page.rb
@@ -0,0 +1,22 @@
+class Avo::Resources::Page < Avo::BaseResource
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :title, as: :text
+    field :slug, as: :text
+    field :content, as: :text
+  end
+
+  def show_fields
+    fields
+
+    field :page_url, as: :text do
+      url_params = {slug: resource.record.slug, id: resource.record}
+      link_to main_app.page_url(url_params), main_app.page_url(url_params), target: "_blank"
+    end
+  end
+end
